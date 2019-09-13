1410de17a5b20834dcb116b85640466d78f645de
Christine Frank <christine.braun13@gmail.com>
2019-09-13 10:34:58 -0700

    Add test form in html and listener in main.js

---
+++ src/main.js
@@ -0,0 +1,13 @@
+
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import './styles.css';
+
+$(document).ready(function(){
+  $('#docSearch').submit(function(event){
+    event.preventDefault();
+
+  });
+});
