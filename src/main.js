3ef7b90c0afbba1bd00d9a7c9be73ec71846aba6
Christine Frank <christine.braun13@gmail.com>
2019-09-13 11:13:26 -0700

    Add functionality to test successful api call

--- src/main.js
+++ src/main.js
@@ -4,10 +4,22 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import './styles.css';
+import {CallDoc} from './js/requestDocInfo.js';
 
 $(document).ready(function(){
   $('#docSearch').submit(function(event){
     event.preventDefault();
+    const searchTerm = $('searchInput').val();
+    const searchType = $('input:radio[name=searchType]:checked').val();
+    //debugger
+    let callDoc = new CallDoc();
+    let results = callDoc.listResults(searchType);
+
+    results.then(function(response){
+      const body = JSON.parse(response);
+      const result = body;
+      console.log(result);
+    });
 
   });
 });
